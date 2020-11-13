718d96f5874f9155b5d842b099811dc5da452d41
chimaogana <clevadani@gmail.com>
2020-11-13 11:45:01 +0100

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<h1>HomePage</h1>
+		<form @submit.prevent="add">
+			<input type="text" v-model="newItem" />
+			<button type="submit">Add</button>
+		</form>
+
+		<items :items="lists"></items>
+	</div>
+</template>
+<script>
+import items from "./Items";
+export default {
+	components: { items },
+	data() {
+		return {
+			lists: [
+				{ name: "Ogana chima" },
+				{ name: "Andrew" },
+				{ name: "Morgan" },
+				{ name: "Vue" },
+				{ name: "Angular" },
+				{ name: "Laravel" }
+			],
+			newItem: null
+		};
+	},
+	methods: {
+		add() {
+			this.lists.push({ name: this.newItem });
+			this.newItem = null;
+		}
+	},
+	created() {
+		EventBu.$on("itemRemove", index => {
+			this.lists.splice(index, 1);
+		});
+	}
+};
+</script>
+
+<style></style>
---
+++ src/components/Items.vue
@@ -0,0 +1,26 @@
+<template>
+	<ul>
+		<li
+			v-for="(item, index) in items"
+			:key="item.name"
+			v-if="item.name !== 'React'"
+		>
+			{{ item.name }}
+			<button @click="remove(index)">X</button>
+		</li>
+		<li v-else>No React Allow here</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	props: ["items"],
+	methods: {
+		remove(index) {
+			return EventBu.$emit("itemRemove", index);
+		}
+	}
+};
+</script>
+
+<style></style>
